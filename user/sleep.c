88e81b2749764963d24f6abc445191a309629199
yao-yin <InYuo1997@gmail.com>
2022-12-10 19:24:37 +0800

    Implement sleep for Lab util

---
+++ user/sleep.c
@@ -0,0 +1,15 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        fprintf(2, "Usage: sleep x\n");
+        exit(1);
+    }
+    int sleep_time = atoi(argv[1]);
+    sleep(sleep_time);
+    exit(0);
+}
